592d3d25fba6b1062f7b862db043ddba18c67bb7
Timofey Elin <elintimofey05@gmail.com>
2024-05-18 18:04:55 +0300

    1

--- Coursework2/Coursework2.cpp
+++ Coursework2/Coursework2.cpp
@@ -4,7 +4,17 @@
 #include <string>
 using namespace std;
 struct Student {
-
+    string FIO;
+    int number;
+    int grades[5];
+    int scholarship;
+    string email;
+    Student(string FIO, int number, int gradesArray[5], int scholarship, string email)
+        : FIO(FIO.substr(0, 50)), number(number), scholarship(scholarship), email(email) {
+        for (int i = 0; i < 5; i++) {
+            this->grades[i] = gradesArray[i];
+        }
+    }
 };
 int main()
 {
